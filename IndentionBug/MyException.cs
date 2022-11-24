b8fb864a45a43a46f85c1f84981736d94d9b7a16
david.schruf <david.schruf@wikifolio.com>
2022-11-24 10:33:01 +0100

    add project to reproduce indention formatting problem

---
+++ IndentionBug/MyException.cs
@@ -0,0 +1,22 @@
+﻿namespace IndentionBug
+{
+    public class MyException : DivideByZeroException
+    {
+        public MyException()
+            : base()
+        {
+        }
+
+        public MyException(string? message)
+        // this should fail the build because indention is not multiple of 4
+              : base(message)
+        {
+        }
+
+        public MyException(string? message, Exception? innerException)
+        // this should fail the build because indention is not multiple of 4
+  : base(message, innerException)
+        {
+        }
+    }
+}
